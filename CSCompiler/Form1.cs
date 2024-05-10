62a57a087bc23abefeb8554542f2ccc85e90a52f
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-05-10 08:33:37 +0300

    Добавьте файлы проекта.

---
+++ CSCompiler/Form1.cs
@@ -0,0 +1,13 @@
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+
+namespace CSCompiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
